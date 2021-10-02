0b61dadd4c9bf010b5c45162f1d539a9207be626
LuanKenobi <68091477+LuanKenobi@users.noreply.github.com>
2021-10-02 16:57:52 -0500

    Adding Materialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id='nav' class="white-text">
+      <router-link to="/">Home</router-link> asdsa|  
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  mounted(){
+    M.AutoInit
   }
 }
 </script>
 
 <style>
+/*@import '../css/materialize.css';*/
+
 #app {
+  background-color: #FFF;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
+

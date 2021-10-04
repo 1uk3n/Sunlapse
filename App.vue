5c110be6f78fb211e210486c150a70f47e66699b
bryan-velasco <86135452+bryan-velasco@users.noreply.github.com>
2021-10-03 20:52:23 -0500

    Bryan's Files
    grax

---
+++ App.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="Space-Weather" style="background-color: #000;">
+    <!--Introduction-->
+    <div class="introduction">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 title">
+            <h2>SPACE WEATHER</h2>
+          </div>
+          <div class="col m12 l5">
+            <div class="video-container">
+              <iframe  width="443" height="315" src="https://www.youtube.com/embed/S00tl1Fh1Hc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+          <div class="col m12 l6 text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum dolor libero illo tempore laudantium aliquam aliquid vero, corrupti, unde explicabo architecto eius error, quisquam quae perspiciatis aperiam atque quasi.
+            Ut odio ab sequi. Animi cupiditate ea cumque dignissimos amet deserunt voluptatum quaerat et enim porro voluptates tenetur adipisci facilis minima sunt, laboriosam corrupti dolore, nam esse laborum debitis perspiciatis.
+            Unde, officia? Explicabo vel rem excepturi accusamus autem quae facere animi perferendis deleniti delectus esse nemo dolores reprehenderit, maiores laborum aliquid veniam eveniet vitae rerum distinctio! Quas accusantium excepturi ducimus.
+          </div>
+          <div class="col s12">
+            <div class="video-container">
+              <iframe src="https://www.solarsystemscope.com/iframe" width="500" height="400" frameborder="5" style="min-width:500px; min-height: 400px; border: 2px solid #0f5c6e;"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--In the solar system-->
+    <div class="in-solar-system">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 title">
+            <h3>IN THE SOLAR SYSTEM</h3>
+          </div>
+          <div class="col s12">
+            <div class="col s6 l3"><img src="../src/assets/mercury.png" class="responsive-img"></div>
+            <div class="col s6 l3"><img src="../src/assets/venus.png" class="responsive-img"></div>
+            <div class="col s6 l3"><img src="../src/assets/earth.png" class="responsive-img"></div>
+            <div class="col s6 l3"><img src="../src/assets/mars.png" class="responsive-img"></div>
+          </div>
+          <div class="col s12 l6 text" style="color: #FFF;">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aspernatur a beatae omnis amet exercitationem praesentium adipisci? Nesciunt sapiente ab velit quaerat ut fugiat. Corrupti perspiciatis nulla veniam tempora distinctio.
+            Corporis nemo quo labore placeat ratione voluptatem, consectetur culpa reiciendis sint cumque nobis saepe quaerat consequatur dolorum earum atque harum. Placeat sunt qui sapiente impedit in ullam? Porro, perspiciatis laborum.
+            Fugit eius, quod id impedit porro ab dolorem voluptates aperiam dignissimos in commodi voluptate earum praesentium excepturi cum animi eaque, ex obcaecati tempora doloremque? Porro voluptatum quis reprehenderit! Dolorum, dolores.
+          </div>
+          <div class="col s12 l6 text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit porro facilis consequuntur dignissimos aliquam in, aspernatur eos voluptates iusto dolores! Earum consequatur odit excepturi? Architecto numquam nisi perspiciatis veritatis quaerat?
+            Optio corrupti nam in id ipsa nesciunt ratione vel non. Similique, harum beatae inventore adipisci vitae id necessitatibus dignissimos incidunt nemo obcaecati mollitia, nam odit, maiores aspernatur libero a odio.
+            In nostrum quod possimus est earum molestias aliquam nisi perferendis pariatur error sed unde voluptas quos voluptatem debitis quis quasi, facilis fuga eos? Dolorem tenetur eligendi, quod corrupti ex impedit.
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--In the earth-->
+    <!--
+    <div class="in-earth">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 title">
+            <h3>IN THE EARTH</h3>
+          </div>
+          <div class="col s12 text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti illo dolore laboriosam obcaecati minus, consectetur deleniti, qui sed rerum velit architecto minima eius, temporibus modi aperiam quidem repellendus perspiciatis at!
+            Exercitationem illo veritatis tempora maxime asperiores neque odio mollitia, eos commodi, error nisi voluptatibus rem odit at expedita nam. In iure rem sunt sapiente est architecto dolorum debitis quisquam numquam.
+            Sapiente modi dolores ipsam nemo molestiae corrupti itaque, corporis voluptatum qui quidem odio fugiat dolorum iure. Omnis impedit commodi voluptatem. Blanditiis delectus ex assumenda molestias eius recusandae architecto cupiditate consequuntur?
+            Incidunt quos dolorem sed cumque ipsum tenetur veritatis neque commodi saepe sit adipisci voluptatibus nesciunt asperiores laboriosam, quisquam voluptatem magni? Nam consectetur ratione qui in nulla mollitia porro a deleniti!
+            A illum modi voluptates inventore praesentium in beatae dolorem voluptas quo amet natus, consectetur facere cupiditate ab sed et dignissimos? Veritatis, nulla consectetur aut nisi inventore quod atque suscipit ratione.
+          </div>
+          <div class="col s12">
+            <div class="video-container">
+              <iframe src="http://andrewmarsh.com/apps/staging/sunpath3d.html" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>-->
+    <!--How does the space weather affects you?-->
+    <!--
+    <div class="you">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 title">
+            <h3 style="color: #FFF">HOW DOES THE SPACE WEATHER AFFECTS YOU?</h3>
+          </div>
+          <div class="col s12">
+            <div class="carousel" style="z-index: 5;">
+              <a class="carousel-item"><img src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss042e033478.jpg"></a>
+              <a class="carousel-item"><img src="https://ntts-prod.s3.amazonaws.com/t2p/prod/t2media/tops/img/TOP2-248/ARC-17122-1B%20Communication-Satellite-Orbiti-82926365%201388x1050.jpg"></a>
+              <a class="carousel-item"><img src="https://www.nasa.gov/sites/default/files/images/542003main_rbsp-astronaut-orig_full.jpg"></a>
+              <a class="carousel-item"><img src="https://svs.gsfc.nasa.gov/vis/a030000/a030300/a030367/ceres_insol_m_2013-09_print.jpg"></a>
+            </div>
+          </div>
+          <div style="color:" class="col s12 text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, fugit quo inventore sed quod aperiam nesciunt, illo placeat accusamus porro laboriosam non qui incidunt alias? Enim asperiores est minus repellendus.
+            Soluta accusamus, aut ipsam tempore pariatur atque perspiciatis temporibus asperiores amet nobis totam, quibusdam cumque rem odit ducimus error harum animi et, repudiandae illo delectus porro consequatur. Placeat, sed nostrum!
+            Harum veritatis fuga necessitatibus. Expedita aspernatur ducimus rem, numquam maxime, nihil quae saepe corrupti quo nulla modi, inventore ad et enim exercitationem temporibus. Dolorum voluptatem recusandae quis corporis perferendis saepe.
+            Placeat dolores, voluptatum assumenda enim eaque rerum dolorem modi, maxime quibusdam hic temporibus cupiditate soluta quis ipsum perferendis atque veritatis libero alias vitae quia. Architecto quasi autem rerum dolorum minima?
+            Deleniti fugit, nulla amet, quis aliquid commodi molestias cupiditate eos tempore omnis alias, saepe maxime dolor enim quas quo excepturi eveniet? Saepe laudantium voluptate quos sed animi beatae, deserunt ut.
+          </div>
+        </div>
+      </div>
+    </div>-->
+  </div>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import M from 'materialize-css';
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  },
+  mounted() {
+      M.AutoInit();
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500&family=Heebo:wght@300&display=swap');
+
+.title{
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  font-size: xx-large;
+  text-align: center;
+  color: #C69C6D
+}
+
+.text{
+  font-family: 'Heebo', sans-serif;
+  text-align: justify;
+  color: #FFF;
+  backdrop-filter: brightness(0);
+}
+
+
+.introduction{
+  /*background-image: url("https://spaceplace.nasa.gov/spaceweather/sp/spaceweather1.sp.jpg");*/
+  background-repeat: no-repeat, repeat;
+  background-size: cover;
+  background-position: top;
+}
+
+.in-solar-system{
+  /*background-image: url("https://images-assets.nasa.gov/image/PIA03604/PIA03604~medium.jpg");*/
+  background-repeat: no-repeat, repeat;
+  background-size: cover;
+  background-position: top;
+}
+
+.in-earth{
+  /*background-image: url("https://science.nasa.gov/science-pink/s3fs-public/styles/background_image_file_size/public/atoms/2e.Magnetosphere_image.jpg");-*/
+  background-repeat: no-repeat, repeat;
+  background-size: cover;
+  background-position: top;
+}
+
+.you{
+  /*background-image: url("https://content.csbs.utah.edu/~pdennison/geog1100_img/1100_GE_snapshot.jpg");*/
+  background-color: #000;
+  background-repeat: no-repeat, repeat;
+  background-size: contain;
+  background-position: bottom;
+}
+</style>
+
+
